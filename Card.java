2326c3ed49a7664a1db5736e8d7b3ddbf07a4dab
caden <cadenhill029@gmail.com>
2026-01-09 09:11:03 -0500

    Added main method back

--- Card.java
+++ Card.java
@@ -1,4 +1,4 @@
-package MagiccardGame;
+
 
 import java.util.HashMap;
 
---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Card solRing = new Card("Sol Ring","colorless", "Artifact", "add two colorless mana");
+		System.out.println(solRing);
+		
+	}
+
+}
